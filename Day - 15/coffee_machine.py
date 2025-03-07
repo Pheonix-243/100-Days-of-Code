e355fb6350a428cb609f0b261c6653d874075c56
Bright <appiahbright5509@gmail.com>
2025-03-07 13:17:21 +0000

    No changes to commit.

--- Day - 15/coffee_machine.py
+++ Day - 15/coffee_machine.py
@@ -64,7 +64,7 @@ def coffee_machine():
                 else:
                     print("Sorry, that's not enough money. Money refunded.")
         else:
-            print("Invalid selection. Please choose again and again and asddddddddddddddddddddddsfhbdfnhdfhndfddddddddddegfsdkajgfsdghsdkbnvkighsdosdosdosdosdosdosdosdosdosdofhgksdfhoddddddddddddddddddddddddddddddddhsddddddddddddddddddsdfgsddddddddvasssssgggggggggggggvsdbsdddddddddddfbhfdbgfhngjnghddhdddddddddddddddddddgbsdghhdfshddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddgian.")
+            print("Invalid selection. Please choose again and again and asddddddddddddddddddddddsfhbdfnhdfhndfddddddddddegfsdkajgfsdghsdkbnvkighsdosdosdosdosdosdosdosdosdosdofhgksdfhoddddddddddddddddddddddddddddddddhsddddddddddddddddsdfbdegsdbsdfbddsdfgsddddddddvasssssgggggggggggggvsdbsdddddddddddfbhfdbgfhngjnghddhdddddddddddddddddddgbsdghhdfshddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddgian.")
 
 
 coffee_machine()
