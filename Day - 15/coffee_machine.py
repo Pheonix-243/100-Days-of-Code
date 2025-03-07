bce681d5c7f63c144b7eded1790e4b562a6eb71e
Bright <appiahbright5509@gmail.com>
2025-03-07 08:24:42 +0000

    No changes to commit.

--- Day - 15/coffee_machine.py
+++ Day - 15/coffee_machine.py
@@ -64,7 +64,7 @@ def coffee_machine():
                 else:
                     print("Sorry, that's not enough money. Money refunded.")
         else:
-            print("Invalid selection. Please choose again and again.")
+            print("Invalid selection. Please choose again and again and agian.")
 
 
 coffee_machine()
