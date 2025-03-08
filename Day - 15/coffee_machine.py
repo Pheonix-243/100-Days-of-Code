8577c96b54609297d60b0e71256f83bdc65575d7
Bright <appiahbright5509@gmail.com>
2025-03-08 18:20:02 +0000

    Update: 1 additions, 1 deletions

--- Day - 15/coffee_machine.py
+++ Day - 15/coffee_machine.py
@@ -64,7 +64,7 @@ def coffee_machine():
                 else:
                     print("Sorry, that's not enough money. Money refunded.")
         else:
-            print("Invalid selection. Please choose again and again and asddddddddddddddddddddddfdndfsfnsdtswtgsfhbdfnhdfhndfddddddddddegfsdkajgfsdghsdkbnvkighsdosdosdosdosdosdosdosdosdosdofhgksdfhoddddddddddddddddddddddddddddddasdgvasdgaskjasnoasfkkjgasgsdsdddhsddddddddddddddddsd\\\\\\\\\\\fbdegsdbsdfbddsdfgsddddddddvasssssgggggggggggggvsdbsdddddddddddfbhfsdhsswesdbgfhngjnghddhdddddddddddddddddddgbsdghhdfshdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddsdgsdhhhhhhhhhhsrhdddddddddddddddgian.")
+            print("Invalid selection. Please choose again and again and asddddddddddddddddddddddfdndfsfnsdtswtgsfhbdfnhdfhndfddddddddddegfsdkajgfsdghsdkbnvkighsdosdosdosdosdosdosdosdosdosdofhgksdfhoddddddddddddddddddddddsrrdddddddddasdgvasdgaskjasnoasfkkjgasgsdsdddhsddddddddddddddddsd\\\\\\\\\\\fbdegsdbsdfbddsdfgsddddddddvasssssgggggggggggggvsdbsdddddddddddfbhfsdhsswesdbgfhngjnghddhdddddddddddddddddddgbsdghhdfshdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddsdgsdhhhhhhhhhhsrhdddddddddddddddgian.")
 
 
 coffee_machine()
