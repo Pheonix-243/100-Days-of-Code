4e0ebc157828e4c3c307b34e0e9d464f5e436c7c
Bright <appiahbright5509@gmail.com>
2025-03-08 18:46:12 +0000

    No changes to commit.

--- Day - 15/coffee_machine.py
+++ Day - 15/coffee_machine.py
@@ -66,6 +66,10 @@ def coffee_machine():
         else:
             print("Invalid selection. Please choose again and again.")
             print("Invalid selection. Please choose again and again.")
+            print("Invalid selection. Please choose again and again.")
+            print("Invalid selection. Please choose again and again.")
+            print("Invalid selection. Please choose again and again.")
+            print("Invalid selection. Please choose again and again.")
 
 
 coffee_machine()
