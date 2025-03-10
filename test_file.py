a64c61c987a88a83d9468977d816fa525ca9742b
Bright <appiahbright5509@gmail.com>
2025-03-10 17:58:58 +0000

    Update: 2 additions, 5 deletions

--- test_file.py
+++ test_file.py
@@ -1,5 +1,6 @@
 # test_example.py
 
+
 def test_addition():
     assert 1 + 1 == 2
 
@@ -8,13 +9,9 @@ def test_subtraction():
     assert 5 - 3 == 2
 
 
-# def test_failure():
-#     assert 10 / 2 == 4  # This test will fail
-
-
 def test_failure():
     assert 10 / 2 == 5
     assert 10 / 2 == 5587899
     assert 10 / 2 == 5587899
     assert 10 / 2 == 5587899
-    assert 10 / 2 == 5587899558789955878995587899
+    assert 10 / 2 == 55878
