16e0c4026c6089fa100ff87abadebbe7887ab7b2
Bright <appiahbright5509@gmail.com>
2025-03-10 18:35:37 +0000

    Update: 4 additions, 1 deletions

--- test_file.py
+++ test_file.py
@@ -1,12 +1,15 @@
 # test_example.py
 
-
 def test_addition():
     assert 1 + 1 == 2
 
 
 def test_subtraction():
     assert 5 - 3 == 2
+
+
+def test_division():
+    return 5 // 3
 
 
 def test_failure():
