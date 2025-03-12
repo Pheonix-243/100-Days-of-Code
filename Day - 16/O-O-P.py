ab76f2e1c4f7d2b896ee6f73abb2ae986ecb0db9
Bright <appiahbright5509@gmail.com>
2025-03-12 19:30:35 +0000

    Update: 59 additions, 30 deletions

--- Day - 16/O-O-P.py
+++ Day - 16/O-O-P.py
@@ -1,30 +1,59 @@
-# An Object is an instance fo a class which is made of methods and attributes
-# Attributes are variables and methods are functions
-# A class can have multiple objects
-#
-# import turtle
-#
-# from turtle import Turtle, Screen
-#
-# lee = Turtle()
-#
-# screen = Screen()
-# lee.speed(5)
-# lee.shape("square")
-# lee.color("coral")
-# lee.forward(54)
-#
-# print(lee)
-#
-# screen.exitonclick()
+# =========================
+# Day 16: Object-Oriented Programming (OOP) in Python
+# =========================
 
-from prettytable import PrettyTable
+# ------------------------
+# Introduction to OOP
+# ------------------------
 
-table = PrettyTable()
+# Object-Oriented Programming (OOP) be a programming paradigm based on objects.
+# Objects be instances of classes, which be blueprints for creating objects.
 
-table.add_column("Name", ["Ama", "John", "Candy"],)
-table.add_column("School", ["Knust", "UCC", "Umat"],)
+# Example of a simple class:
+class Dog:
+    def __init__(self, name, age):
+        self.name = name  # Attribute
+        self.age = age    # Attribute
 
-table._align = 
+    def bark(self):
+        return "Woof! Woof!"
 
-print(table)
+# Creating an object (instance of the Dog class)
+my_dog = Dog("Buddy", 3)
+print(my_dog.name)  # Output: Buddy
+print(my_dog.bark())  # Output: Woof! Woof!
+
+# ------------------------
+# Class Attributes & Instance Attributes
+# ------------------------
+
+# Class attributes be shared among all instances of a class.
+# Instance attributes be specific to each object.
+
+class Cat:
+    species = "Mammal"  # Class attribute
+
+    def __init__(self, name):
+        self.name = name  # Instance attribute
+
+cat1 = Cat("Whiskers")
+print(cat1.name)  # Output: Whiskers
+print(cat1.species)  # Output: Mammal
+
+# ------------------------
+# Methods in Classes
+# ------------------------
+
+# Methods be functions inside a class.
+# They operate on the object's attributes.
+
+class Car:
+    def __init__(self, brand, model):
+        self.brand = brand
+        self.model = model
+
+    def full_name(self):
+        return f"{self.brand} {self.model}"
+
+my_car = Car("Toyota", "Corolla")
+print(my_car.full_name())  # Output: Toyota Corolla
