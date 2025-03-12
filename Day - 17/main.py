a8a8c9213d049726135b45d9f55c6bb6f2117234
Bright <appiahbright5509@gmail.com>
2025-03-12 19:56:09 +0000

    Update: 1 additions, 1 deletions

--- Day - 17/main.py
+++ Day - 17/main.py
@@ -3,7 +3,7 @@ class User:
 # The __init__ function allows us to craete class constructors
 # It is use to initialse attributs
 # This function is called anytime anew object is created frrm the class!
-Ths self function
+Ths self functionis  al
 user_1 = User()
 user_1.id = 34
 
