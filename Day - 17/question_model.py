40a9dc023824ca472f7dd049a7daae55bb71ac8a
Bright <appiahbright5509@gmail.com>
2025-03-12 21:06:12 +0000

    Update: 12 additions, 1 deletions

--- Day - 17/question_model.py
+++ Day - 17/question_model.py
@@ -1,2 +1,13 @@
+from data import question_data
+
+
 class Question:
-    def __init__(self,quest):
+    def __init__(self ,question, answer):
+        self.question = question
+        self.answer = answer
+
+for text in range(len(question_data) - 1)
+    question = Question(text[text],text[])
+
+
+question_1 = Question("The united state is a developed nation","True")
