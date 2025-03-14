f54dffbe33d44ef69fb0c83b60aec15fd76eaeb0
Bright <appiahbright5509@gmail.com>
2025-03-14 01:32:35 +0000

    Update: 1 additions, 1 deletions

--- check.py
+++ check.py
@@ -10,7 +10,7 @@ class MyClass:  # Missing docstring
         self.a = 1  # Poor variable naming
 
     def method_one(self):  # Missing docstring
-        b = 2  # Poor variable naming
+        b = 42  # Poor variable naming
         print(b)
 
 def another_function():  # Missing docstring
